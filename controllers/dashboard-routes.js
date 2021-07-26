9b74a94a6c9ea6ea673457742f801567d0e6a8be
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-07-26 00:47:27 -0400

    post by user who is logged in created

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { Post, User } = require('../models');
 
-router.get('/', withAuth, (req,res)=> {
+router.get('/', withAuth, async (req,res)=> {
     if(req.session.user_id == 1){
         var loggedIn = false
         if(req.session.expiration <= Date.now()){
@@ -13,7 +14,48 @@ router.get('/', withAuth, (req,res)=> {
     else{
         var loggedIn = true
     }
-    return res.render('dashboard', {loggedIn: loggedIn, signUp:true} )
+    console.log(req.session)
+    const allPostById =  await Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        include: [
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    }).then(data => {
+        // console.log(data[0].createdAt)
+        // var date = new Date(data[0].createdAt)
+        // console.log(date.toLocaleDateString())
+        // console.log(date.toLocaleTimeString())
+        // date = date.toLocaleDateString() + ", at " + date.toLocaleTimeString()
+        // console.group(date)
+        // console.log(data[0].user.username)
+        // console.log(data[0].post)
+        // console.log(data[0].title)
+        var post = []
+        var count = 1
+        data.forEach(element => {
+            var date = new Date(element.createdAt)
+            date = date.toLocaleDateString() + ", at " + date.toLocaleTimeString()
+            post.push({count:count,username:element.user.username,title:element.title,post:element.post, date:date})
+            count++
+        })
+        console.log(post)
+        return post
+    })
+    var countString = ''
+        var count = 1
+        countString += "multiCollapseExample"+count.toString()+ " "
+            count++
+    var post = allPostById[allPostById.length-1]
+    console.log(post)
+
+
+
+    return res.render('dashboard', {loggedIn: loggedIn, signUp:true, post: allPostById} )
 })
 
 module.exports = router
